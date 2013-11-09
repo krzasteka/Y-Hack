e8beddcf9291c2290d4040ba8036f527304ad040
Andrew Krzastek <krzasteka@gmail.com>
2013-11-08 19:57:02 -0500

    Added Buttons
    added needed button to main_activity

--- src/com/example/y_hack/Payment.java
+++ src/com/example/y_hack/Payment.java
@@ -1,21 +1,21 @@
 package com.example.y_hack;
-/*test*/
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class Main extends Activity {
+public class Payment extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_payment);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.payment, menu);
 		return true;
 	}
 
